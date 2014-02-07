1ae31904a48f6c436ef127e9c94432b0c417cbc2
Tommsy64 <thomas.pakh@gmail.com>
2014-02-07 14:45:43 -0500

    Added generate function. Attempted to fix HyperLink

--- src/pytriplecalculator/MainController.java
+++ src/pytriplecalculator/MainController.java
@@ -1,7 +1,13 @@
 package pytriplecalculator;
 
+import java.awt.Desktop;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -17,25 +23,56 @@ public class MainController {
 	private TextField number2TextField;
 
 	@FXML
-	private Button createButton;
-
-	@FXML
-	private Button generateButton;
-
-	@FXML
 	protected void generateTriple() {
 
+		try {
+			int number1 = 0;
+			int number2 = 0;
+			while (true) {
+
+				if (Integer.parseInt(number1TextField.getText()) > Integer
+						.parseInt(number2TextField.getText()))
+					break;
+
+				number1 = Integer.parseInt(number1TextField.getText())
+						+ (int) (Math.random() * ((Integer
+								.parseInt(number2TextField.getText()) - Integer
+								.parseInt(number1TextField.getText())) + Integer
+								.parseInt(number1TextField.getText())));
+				number2 = Integer.parseInt(number1TextField.getText())
+						+ (int) (Math.random() * ((Integer
+								.parseInt(number2TextField.getText()) - Integer
+								.parseInt(number1TextField.getText())) + Integer
+								.parseInt(number1TextField.getText())));
+
+				if (number2 > number1)
+					break;
+			}
+			printTriple(number1, number2);
+		} catch (NumberFormatException e) {
+			output.setText("Number 1 is the minimum, Number 2 is the maximum");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
 	protected void createTriple() {
 		try {
-			TripleCreator tCreator = new TripleCreator(
-					Double.parseDouble(number1TextField.getText()),
+			printTriple(Double.parseDouble(number1TextField.getText()),
 					Double.parseDouble(number2TextField.getText()));
+		} catch (NumberFormatException e) {
+			output.setText("Please input a number");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-			if (Double.parseDouble(number1TextField.getText()) < Double
-					.parseDouble(number2TextField.getText())) {
+	private void printTriple(double number1, double number2) {
+		try {
+			TripleCreator tCreator = new TripleCreator(number1, number2);
+
+			if (number1 < number2) {
 				String[] tripleStringArray = tCreator.toStringArray();
 
 				String outputText = "";
@@ -49,6 +86,30 @@ public class MainController {
 
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	protected void openChessClient(ActionEvent event) {
+		try {
+			openWebpage(new URL("http://tommsy64.github.io/Chess-Client/")
+					.toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void openWebpage(URI uri) {
+		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop()
+				: null;
+		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+			try {
+				desktop.browse(uri);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
